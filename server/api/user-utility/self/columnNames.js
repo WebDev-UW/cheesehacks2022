27e70ffce2cce5c8e637b03d292d069e42bd0e87
thok-shock <rswerner@wisc.edu>
2022-10-24 17:30:35 -0500

    started registration server call

---
+++ server/api/user-utility/self/columnNames.js
@@ -0,0 +1,18 @@
+module.exports = {
+    id: 'id',
+    google_id: 'google_id',
+    first_name: 'first_name',
+    last_name: 'last_name',
+    profile_picture_url: 'profile_picture_url',
+    description: 'description',
+    admin: 'admin',
+    website: 'website',
+    email: 'email',
+    dietary_restriction: 'dietary_restriction',
+    vegan: 'vegan',
+    vegetarian: 'vegetarian',
+    lactose_intolerant: 'lactose_intolerant',
+    gluten_free: 'gluten_free',
+    additional_diet: 'additional_diet',
+    registered: 'registered'
+}
--- server/api/user-utility/self/router.js
+++ server/api/user-utility/self/router.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const db = require('../../../db')
+const updateQueryGenerator = require('../../../updateQuery')
+const columnNames = require('./columnNames')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
     // #swagger.tags = ["user-utility/self"]
     // #swagger.summary = "Load information about yourself"
-    console.log(req.session.passport)
+    //console.log(req.session.passport)
     if (req.session && req.session.passport && req.session.passport.user.id) {
         db.query(`SELECT * FROM user_entry WHERE id = ?`, [req.session.passport.user.id], (err, rows) => {
             err ? res.sendStatus(401) : res.json(rows)
         })
     } else {
         res.sendStatus(404)
+    }
+})
+
+router.put('/', (req, res) => {
+    // #swagger.tags = ['user-utility/self']
+    // #swagger.summary = "Modify information about yourself"
+    if (req.session.passport && req.session.passport.id && req.body) {
+        const clientInfo = req.body
+        delete clientInfo.admin
+        delete clientInfo.google_id
+        const {query, rows} = updateQueryGenerator('user_entry', columnNames, clientInfo)
     }
 })
 
